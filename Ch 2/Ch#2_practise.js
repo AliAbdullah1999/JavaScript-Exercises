bc00ea90c8dfe979fc33bf7295f9012d3734eaea
Ali Abdullah Bhatti <53481567+AliAbdullah1999@users.noreply.github.com>
2023-07-31 17:35:45 +0000

    Add files via upload

---
+++ Ch 2/Ch#2_practise.js
@@ -0,0 +1,16 @@
+//Question#1
+//increment and decrement
+a=50
+console.log('a',a)
+
+//increment
+console.log('++a',++a)
+console.log('a',a)
+console.log('a++',a++)
+console.log('a',a)
+
+//decrement
+console.log('--a',--a)
+console.log('a',a)
+console.log('a--',a--)
+console.log('a',a)
