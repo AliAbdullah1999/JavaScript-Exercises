41de61d57f47670407d1346bec65d30932bdb1f1
AliAbdullah1999 <aliabdullahpk1@gmail.com>
2023-09-12 17:52:50 +0000

    New changes

---
+++ Exercises/2.15/functions-tasks.js
@@ -0,0 +1,50 @@
+// function sum(a,b = "No number entered"){
+//     console.log(a+b)
+// }
+// sum(4)
+// let age = 18;
+// let s = 'this is innerone';
+// Task#1
+// It works normally how it should
+//       function checkAge(age) {
+//         if (age > 18) {
+//           return console.log(true)
+//         } else {
+//           console.log('Did you ask for your parents permission')
+//         }
+//       }
+//       checkAge(19)
+// Without else
+//Yes! it will work normally as it works with else
+// function checkAge(age){
+//     if(age>18){
+//         return console.log(true)
+//     }
+//     console.log('Did you as for your parents permission')
+// }
+// checkAge(33)
+// Task#2
+//using ? 
+// function checkAge(age){
+//     console.log(age>18 ? true : 'Did you get your parents permission')
+// }
+// checkAge(15)
+//using ||
+// function checkAge(age){
+//    return console.log( (age > 18)  || ('Did your ask for your parents permission'));
+// }
+// checkAge(13)
+// Task#3
+// function min(a,b){
+//     if (a>b){
+//         return b
+//     }else{
+//         return a
+//     }
+// }
+// console.log(min(5,3))
+// Task#4
+// function power(x,n){
+//     return x**n
+// }
+// console.log(power(3,3))
